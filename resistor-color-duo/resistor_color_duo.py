e5eb851cda9500945baee9cad71812d96ae13227
mstrzelczyk4 <58236066+mstrzelczyk4@users.noreply.github.com>
2020-02-25 23:35:37 +0100

    Add files via upload

---
+++ resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,16 @@
+code = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
+
+
+def value(colors):
+    return code[colors[0]] * 10 + code[colors[1]]
